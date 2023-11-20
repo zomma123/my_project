bf4a479c21f7165c0917f6f96e254ff9c62b3338
zomma123 <zommaid2003@gmail.com>
2023-11-20 18:13:56 +0700

    upload files

---
+++ resources/js/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1> ໜ້າຫຼັກ </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyWebappApp',
+
+  data() {
+    return {
+      
+    };
+  },
+
+  mounted() {
+    
+  },
+
+  methods: {
+    
+  },
+
+  computed: {
+    
+  },
+
+  watch: {
+    
+  },
+
+  created() {
+    
+  },
+
+  beforeRouteEnter (to, from, next) {
+    // ...
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My WebApp</title>
+
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}" type="text/css" >
+</head>
+<body>
+    <div id="app-vue" ></div>
+
+    <script src="{{ mix('js/app.js') }}" type="text/javascript"></script>
+</body>
+</html>
